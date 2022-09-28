a5e49b73e06f6da1a978296c08dee556575b7f4f
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-28 21:39:56 +0200

    pygame initialization

---
+++ solver.py
@@ -0,0 +1,6 @@
+import pygame # used for the display
+
+pygame.font.init()
+screen = pygame.display.set_mode((1000, 1000)) #create 1000px x 1000px window 
+
+pygame.display.set_caption("Sudoku solver")
