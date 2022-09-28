8087ca751a6e733e3430c4ad08d9d01ab8a65aee
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-28 21:50:16 +0200

    created function to draw board

--- solver.py
+++ solver.py
@@ -1,6 +1,30 @@
-import pygame # used for the display
+import pygame  # used for the display
 
-pygame.font.init()
-screen = pygame.display.set_mode((1000, 1000)) #create 1000px x 1000px window 
+pygame.init()
+screen = pygame.display.set_mode((1000, 1000))  # create 1000px x 1000px window
+
 
 pygame.display.set_caption("Sudoku solver")
+
+grid = [[0 for _ in range(9)] for x in range(9)]  # sudoku grid is 9x9
+
+step = 1000/9
+
+
+def drawGrid():  # this isn't a pure function but oh well
+    screen.fill((255, 255, 255))
+    for i in range(9)[1:]:
+        if i % 3 == 0:
+            pygame.draw.line(screen, (0, 0, 0), (i*step, 0), (i*step, 1000), 3)
+        else:
+            pygame.draw.line(screen, (0, 0, 0), (i*step, 0), (i*step, 1000), 1)
+    for i in range(9)[1:]:
+        if i % 3 == 0:
+            pygame.draw.line(screen, (0, 0, 0), (0, i*step), (1000, i*step), 3)
+        else:
+            pygame.draw.line(screen, (0, 0, 0), (0, i*step), (1000, i*step), 1)
+
+
+while True:
+    drawGrid()
+    pygame.display.update()
